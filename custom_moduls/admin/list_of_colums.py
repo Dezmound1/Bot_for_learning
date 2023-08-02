b0eb399a8adc14529fb779ea2504f4096bf89cc7
Dezmound1 <Dezmoynd@mail.ru>
2023-08-03 03:05:33 +1000

    df

---
+++ custom_moduls/admin/list_of_colums.py
@@ -0,0 +1,23 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import sqlite3
+conn = sqlite3.connect('Data.db')
+cursor = conn.cursor()
+table_name = 'subject'
+cursor.execute(f"PRAGMA table_info({table_name})")
+columns = cursor.fetchall()
+
+column_names = [column[1] for column in columns[3:]]
+conn.close()
+
+column_dict = {
+    'cost': 'стоимость',
+    'schedule': 'дата',
+    'room': 'аудитория',
+    'teacher': 'преподаватель',
+    'comment': 'комментарий'
+}
+translated_dict = {column_name: column_dict[column_name] for column_name in column_names}
+button = []
+for element in translated_dict:
+    button.append([InlineKeyboardButton(text = translated_dict[element], callback_data= element)])
+changing = InlineKeyboardMarkup(row_width=1, resize_keyboard=True, inline_keyboard= button)
